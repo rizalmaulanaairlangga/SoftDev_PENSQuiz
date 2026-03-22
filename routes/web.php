6f91bfb53434410bcb3bc83e4781f0492959bf9d
Rizal <rizalmaulanaairlanggad4itb2024@gmail.com>
2026-03-22 21:41:56 +0700

    feat(frontend): setup frontend architecture and implement landing page

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,32 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn() => view('pages.public.landing'));
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])->group(function () {
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', fn() => view('pages.dashboard.index'))
+        ->name('dashboard');
+
+    // Route::get('/quizzes', [QuizController::class, 'discover']);
+    // Route::get('/quizzes/{id}', [QuizController::class, 'show']);
+
+    // Route::get('/quiz/create', [QuizController::class, 'create']);
+    // Route::post('/quiz', [QuizController::class, 'store']);
+
+    // Route::get('/quiz/{id}/questions', [QuestionController::class, 'index']);
+
+    // Route::post('/quiz/{id}/start', [AttemptController::class, 'start']);
+    // Route::get('/attempt/{id}', [AttemptController::class, 'play']);
+
+    // Route::get('/attempt/{id}/result', [AttemptController::class, 'result']);
+
+    // Route::get('/history', [AttemptController::class, 'history']);
+
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+
+    // Route::get('/history', [AttemptController::class, 'history']);
 });
 
 require __DIR__.'/auth.php';
